a86fe1dae22efdd4c3c35c6fa4d97178be818b7d
likevin97 <kl553@cornell.edu>
2018-10-11 16:21:16 -0400

    Actually added transition probabilities, not just counts

--- tagToTag.py
+++ tagToTag.py
@@ -1,27 +1,42 @@
 from collections import defaultdict
 
-TAGS = ["B-PER", "B-LOC", "B-ORG", "B-MISC", "I-PER", "I-LOC", "I-ORG", "I-MISC", "O"]
 
-
-def transition_probabilities(fname):
-    train = open(fname, "r")
-    trans_dict = defaultdict(lambda: defaultdict(int))
+def transition_counts(filename):
+    train = open(filename, "r")
+    trans_counts = defaultdict(lambda: defaultdict(int))
     counter = 0
     for line in train:
         # You are on the BIO tag line
-        if counter%3 == 2:
+        if counter % 3 == 2:
             line_array = line.split()
             for bioTagIndex in range(1, len(line_array)):
-                prevTag = line_array[bioTagIndex - 1]
-                currTag = line_array[bioTagIndex]
-                trans_dict[prevTag][currTag] += 1
+                prev_tag = line_array[bioTagIndex - 1]
+                curr_tag = line_array[bioTagIndex]
+                trans_counts[prev_tag][curr_tag] += 1
         counter += 1
-    return trans_dict
+    return trans_counts
+
+
+def transition_probabilities(trans_counts):
+    trans_probs = defaultdict(lambda: defaultdict(float))
+
+    for prevTagKey in trans_counts.keys():
+        prev_tag_dict = trans_counts[prevTagKey]
+        prev_total = sum(prev_tag_dict.values())
+
+        for currTagKey in prev_tag_dict.keys():
+            trans_probs[prevTagKey][currTagKey] = float(trans_counts[prevTagKey][currTagKey]) / prev_total
+
+    return trans_probs
+
 
 def main():
 
     print ("Dictionary Output: ")
-    transitionProbabilities =  (transition_probabilities("training.txt"))
+    transition_counts_output = transition_counts("sample.txt")
+
+    transition_prob_output = transition_probabilities(transition_counts_output)
+
     print ("End debug mode")
 
 
